374531453a6045c8a1d09fd14d0b817d4514ec62
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-16 13:58:56 +0200

    Added components, context and views

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,12 @@
 import React from "react";
+import logoImg from "../assets/Logo.png"
 
 function Header(){
     return(
+        <React.Fragment>
+        <img src={logoImg} alt="Logo" className="headerLogo"></img>
         <div>Header component</div>  
+        </React.Fragment>
     )  
 }
 
