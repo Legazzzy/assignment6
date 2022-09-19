efbd621db78e23de8d2ce295969ef082f1531a99
LinneaJohansen <linnea_johansen@outlook.com>
2022-09-19 14:02:20 +0200

    Added withAuth

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 
+import withAuth from "../hoc/withAuth";
 import ProfileActions from "../components/Profile/ProfileActions";
 import ProfileHeader from "../components/Profile/ProfileHeader";
 import ProfileTranslateHistory from "../components/Profile/ProfileTranslateHistory";
@@ -10,13 +12,13 @@ const Profile = () => {
     const { user } = useUser();
 
     return (
-        <>
+        <div>
           <h1>Profile</h1>
-          <ProfileHeader username={ "username" }/>
+          <ProfileHeader username={ user.username }/>
           <ProfileActions />
-          <ProfileTranslateHistory translations={ "user.translations" }/>
-        </>
+          <ProfileTranslateHistory translations={ user.translations }/>
+        </div>
     )
 }
 
-export default Profile
+export default withAuth(Profile);
