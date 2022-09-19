22544f246cb38f65fc5f5965d4f08b2fa4fcb1b6
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-19 12:33:12 +0200

    fixed login page

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import logoImg from "../../assets/Logo.png"
+import '../../css/login.css';
+import 'animate.css';
+var usernames = [];
 
 
 function Login(){
     return(
         <React.Fragment>
-        <div>Login</div>
-        <img src={logoImg} alt="Logo" className="headerLogo"></img>
+        <div className="loginDiv">
+        <img src={logoImg} alt="Logo" className="headerLogo animate__animated animate__bounceInLeft"></img>
+        <div className="inputDiv">
+            <form onSubmit="checkForUsername()">
+                let username = <input type="text" className="username" name="username" placeholder="Username"/>
+                <button type="Submit"> Submit</button>
+            </form>
+        </div>
+        </div>
+        
         </React.Fragment>
     )
+}
+
+function checkForUsername() {
+    prompt("hello")
+    let username = document.getElementsByClassName("username").value;
+    prompt("username")
+    if (usernames.has(username)){
+
+    }
 }
 
 export default Login;
