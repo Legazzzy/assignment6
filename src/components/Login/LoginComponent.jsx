7e668863928e69ab42ce870d02ee7d86a16bb8ea
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-19 15:48:55 +0200

    added loggin that checks up with api

--- src/components/Login/LoginComponent.jsx
+++ src/components/Login/LoginComponent.jsx
@@ -10,25 +10,11 @@ function Login(){
         <React.Fragment>
         <div className="loginDiv">
         <img src={logoImg} alt="Logo" className="headerLogo animate__animated animate__bounceInLeft"></img>
-        <div className="inputDiv">
-            <form onSubmit="checkForUsername()">
-                let username = <input type="text" className="username" name="username" placeholder="Username"/>
-                <button type="Submit"> Submit</button>
-            </form>
-        </div>
         </div>
         
         </React.Fragment>
     )
 }
 
-function checkForUsername() {
-    prompt("hello")
-    let username = document.getElementsByClassName("username").value;
-    prompt("username")
-    if (usernames.has(username)){
-
-    }
-}
-
 export default Login;
+
