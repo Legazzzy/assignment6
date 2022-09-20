c3ab101776ff1383693d58b0b25ad2723bce4147
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-20 09:41:04 +0200

    fixed login button to be disabled when clicked

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import '../css/login.css';
 import LoginComp from '../components/Login/LoginComponent'
 import LoginForm from '../components/Login/LoginForm'
-import Profile from '../views/Profile'
-
-import { Link } from "react-router-dom";
-
 
 const Login = () => {
     return (
         <React.Fragment>
-        <Link to="/profile">profile</Link>  
         <LoginComp />
         <LoginForm />
         </React.Fragment>
