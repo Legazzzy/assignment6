4e8090a4de09180a346986d938d2ead8c8193ee0
LinneaJohansen <linnea_johansen@outlook.com>
2022-09-20 09:04:02 +0200

    Added api scripts

---
+++ src/api/user.js
@@ -0,0 +1,52 @@
+import { createHeaders } from './index'
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+
+export const checkForUser = async (username) => {
+    try {
+        const response = fetch('${apiUrl}?username=${username}');
+        if(!response.ok) {
+            throw new Error('Could not complete request');
+        }
+        const data = await response.json();
+        return [null, data];
+    } catch (error) {
+        return [error.message, null];
+    }
+}
+
+export const createUser = async (username) => {
+    try {
+        const response = fetch(apiUrl, {
+            method: 'POST',
+            headers: createHeaders(),
+            body: JSON.stringify({
+                username,
+                translations: []
+            })
+        });
+        if(!response.ok) {
+            throw new Error('Could not create new user with username '+username);
+        }
+        const data = await response.json();
+        return [null, data];
+    } catch (error) {
+        return [error.message, null];
+    }
+}
+
+export const loginUser = async (username) => {
+    const [ checkError, user ] = await checkForUser(username);
+
+    if(checkError !== null) {
+        return [ checkError, null ];
+    }
+
+    if (user.length > 0){
+        //User exist
+        return [ null, user.pop() ];
+    }
+
+    return await createUser(username);
+}
