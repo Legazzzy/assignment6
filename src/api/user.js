45f7113823d73364e1369c150b21d6abf1412825
Legazzzy <marius.eriksen13@yahoo.no>
2022-09-23 13:13:52 +0200

    added commenting

--- src/api/user.js
+++ src/api/user.js
@@ -1,15 +1,20 @@
 import { createHeaders } from './index'
 
-const apiUrl = process.env.REACT_APP_API_URL+"/translations"
+const apiUrl = process.env.REACT_APP_API_URL
 
+//check if user exists
 export const checkForUser = async (username) => {
     try {
+        //wait for response from API with the username
         const response = await fetch(`${apiUrl}?username=${username}`);
         console.log(response)
+        //if response isn't ok, error
         if(!response.ok) {
             throw new Error('Could not complete request');
         }
+        //transforms data to JSON
         const data = await response.json();
+        //return user object
         return [null, data];
     }
     catch (error) {
@@ -17,8 +22,10 @@ export const checkForUser = async (username) => {
     }
 }
 
+//if no user with username, creat new
 export const createUser = async (username) => {
     alert("no user with this name, creating user")
+    //post new user with username
     try {
         const response = await fetch(apiUrl, {
             method: 'POST',
@@ -32,25 +39,30 @@ export const createUser = async (username) => {
             throw new Error('Could not create new user with username '+username);
         }
         const data = await response.json();
+        //return user object
         return [null, data];
     } catch (error) {
         return [error.message, []];
     }
 }
 
+//log in user
 export const loginUser = async (username) => {
     alert("logging in")
 
+    //check if user exists
     const [checkError, user] = await checkForUser(username)
 
+    //error handling
     if (checkError !== null) {
         return [ checkError, null ]
     }
 
+    //if if there is a user with the username, return user
     if (user.length > 0){
         return [ null, user.pop() ];
     }
-
+    // if no user with username, return createUser with the given username
     return await createUser(username);
 
 }
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -6,6 +6,8 @@ import { useUser } from '../../context/UserContext';
 import { loginUser } from '../../api/user';
 import '../../css/login.css';
 
+
+//requirements for username
 const usernameConfig = {
     required: true,
     minLength: 2
@@ -21,38 +23,50 @@ const LoginForm = () => {
 
     const { user, setUser} = useUser()
 
-    
+    //state for loading boolean
     const [loading, setLoading] = useState(false)
+
+    //state for apiError
     const [apiError, setApiError] = useState(null)
+
+    //redirect/navigation
     const nav = useNavigate();
 
 
+    //when user has been checked
     useEffect(() => {
         console.log('user has changed!' + user)
+
+        //if user is something else than null, it means it has been changed
         if (user !== null){
+            //redirect to /translations
             nav('/translations')
         }
     }, [ user, nav ])
 
-    /**
-     * Takes in written username and disables the login button
-     * Then sends username to loginUser to validate if it's an existing user or new user
-     * If there is an error, display it.
-     * If user is not null, it means that responseUser has changed, and is now the user
-     * Set user to respionseUser.
-     * Enables button again
-     */
+
+    //takes in async username
     const onSubmit = async ({ username }) => {
+        //button has been pressed, set loading to true
         setLoading(true)
+
+        //login user, wait for response, rsponse will be an object
         const [error, responseUser] = await loginUser(username)
         console.log("response: "+responseUser)
+
+        //if there is an error, set the api error
         if (error !== null) {
             setApiError(error)
         }
+        // if there is no error
         if (responseUser !== null){
+            //save username from response object
             storageSave('logged in user', responseUser.username)
+            //set user to response object
             setUser(responseUser)
         }
+
+        //set loading to false when user has been logged in.
         setLoading(false)
     };
 
